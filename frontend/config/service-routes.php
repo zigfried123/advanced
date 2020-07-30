4bbb3117e9d28000fb0fe31527594b14223e4419
Max Protodyakonov <zigfried123@mail.ru>
2020-07-30 13:32:25 +0400

    fix regular expression to city

--- frontend/config/service-routes.php
+++ frontend/config/service-routes.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'api/v1/service/get-services-by-city/<city:[a-z]+|[a-z]+-[a-z]+>' => 'api/service/get-services-by-city',
+    'api/v1/service/get-services-by-city/<city:[a-z]+(-[a-z]+)?>' => 'api/service/get-services-by-city',
     'api/v1/service/get-service-by-id/<id:\d+>' => 'api/service/get-service-by-id',
 ];
